95a382d6b61ea5872d7a2e3bbf458eca0addd934
RaphaelDeLaGhetto <daniel@capitolhill.ca>
2015-04-02 15:02:52 -0600

    Enable passenger restart with sudo and clean up

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,25 +2,10 @@
 lock '3.4.0'
 
 set :application, 'rails_tutorial_template'
-set :repo_url, 'git@github.com:RaphaelDeLaGhetto/rails-tutorial-template.git'
-
-# Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+set :repo_url, 'https://github.com/RaphaelDeLaGhetto/rails-tutorial-template'
 
 # Default deploy_to directory is /var/www/my_app_name
 set :deploy_to, '/home/deploy/rails-tutorial-template'
-
-# Default value for :scm is :git
-# set :scm, :git
-
-# Default value for :format is :pretty
-# set :format, :pretty
-
-# Default value for :log_level is :debug
-# set :log_level, :debug
-
-# Default value for :pty is false
-# set :pty, true
 
 # Default value for :linked_files is []
 set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
@@ -28,12 +13,9 @@ set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/
 # Default value for linked_dirs is []
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
 
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+set :passenger_restart_with_sudo, true
 
-# Default value for keep_releases is 5
-# set :keep_releases, 5
-
+# deploy
 namespace :deploy do
 
   desc 'Restart application'
@@ -45,14 +27,4 @@ namespace :deploy do
 
   after :publishing, 'deploy:restart'
   after :finishing, 'deploy:cleanup'
-
-#  after :restart, :clear_cache do
-#    on roles(:web), in: :groups, limit: 3, wait: 10 do
-#      # Here we can do anything such as:
-#      # within release_path do
-#      #   execute :rake, 'cache:clear'
-#      # end
-#    end
-#  end
-
 end
