d387ead4c6f0986f8f600118341759aeb8333147
RaphaelDeLaGhetto <daniel@capitolhill.ca>
2015-04-13 10:58:13 -0600

    Add Apps route and view

--- config/routes.rb
+++ config/routes.rb
@@ -8,12 +8,10 @@ Rails.application.routes.draw do
   get 'users/new'
 
   root 'static_pages#home'
-
   get 'help' => 'static_pages#help'
-
   get 'about' => 'static_pages#about'
-
   get 'contact' => 'static_pages#contact'
+  get 'apps' => 'static_pages#apps'
 
   get 'signup' => 'users#new'
 
